442971cc844feeddd5d91fb95af57e76391d9400
mahdi <mjalalimech@gmail.com>
2024-10-26 11:59:50 +0400

    intalled vite

---
+++ README.md
@@ -0,0 +1,8 @@
+Shopping Cart of a store:
+
+1. Front-end (client): React 
+UI: React-bootstrap
+Global Data management: context
+
+2. Back-end (API): Node + Express
+Data: Mombu
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+function App() {
+  
+  return (
+    <>
+      <h5 className="text-success">Shopping Cart</h5>
+    </>
+  )
+}
+
+export default App
